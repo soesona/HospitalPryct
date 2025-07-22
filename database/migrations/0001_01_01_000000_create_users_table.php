07e9faa399f2eed7842a0f132d2bd057a57320e6
soesona <usthm_sosae@unicah.edu>
2025-07-22 00:33:13 -0600

    probandooo

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->timestamps();
         });
 
+
     
   Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
@@ -31,6 +32,12 @@ return new class extends Migration
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
+
+         Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
     }
 
     /**
@@ -39,7 +46,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-
+           Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
 };
