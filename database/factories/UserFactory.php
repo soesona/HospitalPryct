d1c47b31c911672b407dce14b766c67ba5caae84
soesona <usthm_sosae@unicah.edu>
2025-07-24 01:20:45 -0600

    otra vez

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,14 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'nombreCompleto' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'identidad' => $this->faker->unique()->numerify('0801#########'),
+            'fechaNacimiento' => $this->faker->date('Y-m-d', '2005-01-01'),
+            'telefono' => $this->faker->numerify('9#######'),
+            'is_active' => true,
             'remember_token' => Str::random(10),
         ];
     }
